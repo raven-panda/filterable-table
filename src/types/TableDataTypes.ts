3fd4c2ca39a2228b9215506fcfc8d0f1b67ea1b1
raven-panda <ambroise.maire7@gmail.com>
2025-12-05 17:45:01 +0100

    docs: added JS documentation to types

--- src/types/TableDataTypes.ts
+++ src/types/TableDataTypes.ts
@@ -2,10 +2,16 @@ export type SortDirection = "asc" | "desc" | "none";
 export type TableColumnDataType = "string" | "number" | "date";
 
 export interface FilterableTableColumn<TDataKey = string> {
+  /** Column display name */
   name: string;
+  /** Column data key, used to access the corresponding value in data objects */
   dataKey: TDataKey;
+  /** Column data type, used for sorting and formatting. If not set, fallbacks to "string" */
   dataType?: TableColumnDataType;
+  /** If true, disables sorting for this column */
   disableSorting?: boolean;
+  /** If true, disables searching for this column */
+  disableSearching?: boolean;
 }
 export interface FilterableTableData {
   values: {
